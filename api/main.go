34702fe42a445b4229a60c740ace4c1b11015bae
mikeunge <mikeunge@protonmail.com>
2022-02-05 12:55:24 +0100

    add: docker-compose for api, database and adminer is setup; dockerfile creates AND realoads api on changes (watches *.go files)

--- api/main.go
+++ api/main.go
@@ -7,15 +7,16 @@ import (
 	"tasker-api/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
 	_, err := database.Connect()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not connect to database.\n\nError: %v\n", err)
 		os.Exit(1)
 	}
-
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -24,7 +25,8 @@ func main() {
 
 	port, err := utils.GetEnv("APP_PORT")
 	if err != nil {
-		port = "3033"
+		fmt.Fprintf(os.Stderr, "APP_PORT not specified or not able to load from .env, using fallback :: 3030")
+		port = "3030"
 	}
 
 	app.Listen(":" + port)
